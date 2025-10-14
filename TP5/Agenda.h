6731ce10b7949e74ba7e1d63b4609cf903343f97
Raouf Nechmi <152798689+raoufne@users.noreply.github.com>
2025-10-14 10:52:18 +0200

    Add Agenda class header file
    Introduces Agenda.h defining the Agenda class with methods for adding, removing, displaying, and clearing appointments. Includes constructors, destructor, and uses NoeudRDV for internal representation.

---
+++ TP5/Agenda.h
@@ -0,0 +1,24 @@
+#ifndef AGENDA_H
+#define AGENDA_H
+
+#include "NoeudRDV.h"
+#include <iostream>
+using namespace std;
+
+class Agenda {
+private:
+    NoeudRDV* premier;
+
+public:
+    Agenda();
+    Agenda(const Agenda& other);
+    ~Agenda();
+
+    void ajoute(const RDV& r);       
+    void enleve();                    
+    void enleve(const RDV& r);       
+    void affiche() const;
+    void vider();                    
+};
+
+#endif
