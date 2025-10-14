b2f706099ef4387ed85cc253fa3b4afc73331d09
Raouf Nechmi <152798689+raoufne@users.noreply.github.com>
2025-10-14 10:17:52 +0200

    Update Heure.h

---
+++ TP4/Heure/Heure.h
@@ -0,0 +1,26 @@
+#ifndef HEURE_H
+#define HEURE_H
+
+#include <iostream>
+using namespace std;
+
+class Heure {
+private:
+    int h, min;
+    bool checkHeure(int h, int min);
+
+public:
+    Heure();                   
+    Heure(int h, int min);     
+
+    int getH() const;
+    int getMin() const;
+
+    void setH(int h);
+    void setMin(int min);
+
+    void saisir();
+    void affiche() const;
+};
+
+#endif
