716d4299611421e5d36f48eea7bd66049dacc77f
Raouf Nechmi <152798689+raoufne@users.noreply.github.com>
2025-12-02 15:50:09 +0100

    Move Vecteur method definitions inside class
    Refactored the Vecteur template class by moving all method definitions from outside the class to inside the class declaration in the header file. This simplifies the code and improves readability.

--- TP9/Ex3/Vecteur.h
+++ TP9/Ex3/Vecteur.h
@@ -10,35 +10,25 @@ class Vecteur {
     int n;
 
 public:
-    Vecteur(int n);
-    ~Vecteur();
-
-    T& operator[](int i);
-    int size() const;
-};
-
-template <class T>
-Vecteur<T>::Vecteur(int n) : n(n) {
-    data = new T[n];
-}
-
-template <class T>
-Vecteur<T>::~Vecteur() {
-    delete[] data;
-}
-
-template <class T>
-T& Vecteur<T>::operator[](int i) {
-    if (i < 0 || i >= n) {
-        cout << "Erreur : indice hors limites !" << endl;
-        exit(1);
+    Vecteur(int n) : n(n) {
+        data = new T[n];
     }
-    return data[i];
-}
 
-template <class T>
-int Vecteur<T>::size() const {
-    return n;
-}
+    ~Vecteur() {
+        delete[] data;
+    }
+
+    T& operator[](int i) {
+        if (i < 0 || i >= n) {
+            cout << "Erreur : indice hors limites !" << endl;
+            exit(1);
+        }
+        return data[i];
+    }
+
+    int size() const {
+        return n;
+    }
+};
 
 #endif
